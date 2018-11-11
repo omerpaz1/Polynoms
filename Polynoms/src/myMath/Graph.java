529cd50c28359c18f2d49caf5503d47b226bcf31
shimon1705 <44582779+shimon1705@users.noreply.github.com>
2018-11-12 01:29:42 +0200

    Update Graph.java
    TestFinal

--- Polynoms/src/myMath/Graph.java
+++ Polynoms/src/myMath/Graph.java
@@ -17,8 +17,33 @@ import fr.julien.graphique.element.forme.Polygone;
 import fr.julien.graphique.element.point.Point;
 import fr.julien.graphique.element.quadrillage.Quadrillage;
 
+/**
+ * This source makes it possible to graph all the usual functions by defining the representation window as well as a
+ * large number of geometric objects: polygons, points, segments, circles ...
+ * @author Omer Paz and Shimon Mimoun and Julien_c (Import).
+ */
 public class Graph {
-	
+
+	/**
+	 * Main function used to perform the print sample
+	 * @throws wrongDataException Exception that knows how to handle the String received by Polynom is check if the enter is correct
+	 */
+	public static void main(String[] args) throws wrongDataException {
+
+		String string = "0.2*X^4+(0-1.5)*X^3+3*X^2+(0-X)+(0-5)"; // the right string for the polynom.
+		Polynom poly = new Polynom("0.2X^4-1.5X^3+3X^2-X-5");
+		System.out.println(poly.toString());
+		PrintMinMax(poly,string, -2,6);
+
+
+	}
+	/**
+	 *The function is used the project Import Graph, and call the function that can print a function and the Max and Min points.
+	 * @param poly receiv Polynom for example: 0.2X^4-1.5X^3+3X^2-X-5 
+	 * @param s  Receiv String in the form of a Polynom, expample of right string for Printing: 0.2*X^4+(0-1.5)*X^3+3*X^2+(0-X)+(0-5). 
+	 * @param x0 lower definition domain
+	 * @param x1 higher definition domain
+	 */
 	public static void PrintMinMax(Polynom poly, String s, double x0, double x1) {
 
 		double start = x0;
@@ -39,14 +64,20 @@ public class Graph {
 		Draw_MinMax(s,poly.Point_list,x0,x1);
 
 	}
-
+	/**
+	 * The function is used the project Import Graph, and print a function and the Max and Min points
+	 * @param s  Receiv String in the form of a Polynom, expample of right string for Printing: 0.2*X^4+(0-1.5)*X^3+3*X^2+(0-X)+(0-5). 
+	 * @param point_list Receiv ArrayList with Points Max and Min 
+	 * @param x0 lower definition domain
+	 * @param x1 higher definition domain
+	 */
 	private static void Draw_MinMax(String s, ArrayList<Point> point_list, double x0, double x1) {
 
 		JFrame f = new JFrame();		
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		OptionAxe optionsAxes = new OptionAxe(Color.BLACK, true, -1, 1, true, true);
 		Graphique.getInstance().initGraphique(new AxeX(x0, 10, optionsAxes), new AxeY(-10, x1, optionsAxes));
-		
+
 		char a = 'A';
 		for (int i = 0 ; i < point_list.size(); i++, a++) {
 			Graphique.getInstance().ajouterElement(new Point(a, point_list.get(i).getAbscisse(),point_list.get(i).getOrdonnee()));
@@ -62,7 +93,7 @@ public class Graph {
 
 	}
 
-	
+
 
 
 }
